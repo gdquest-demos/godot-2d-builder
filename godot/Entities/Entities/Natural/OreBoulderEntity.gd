f8c9322bf6b123b20ac366854f9caa980e4ef071
Francois Belair <razoric480@gmail.com>
2020-09-24 22:25:49 -0400

    Add ore and rock boulders

---
+++ godot/Entities/Entities/Natural/OreBoulderEntity.gd
@@ -0,0 +1,13 @@
+extends Entity
+
+const REGIONS := [
+	Rect2(340, 780, 100, 100),
+	Rect2(450, 780, 100, 100),
+]
+
+
+func _ready() -> void:
+	var index := int(rand_range(0, REGIONS.size() - 1))
+	$Sprite.region_rect = REGIONS[index]
+	get_child(index + 1).disabled = false
+	scale.x = 1 if rand_range(0, 10) < 5.5 else -1
